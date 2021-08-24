169db7344babdc5a56b47e97b6b5be01f5b3fd92
Chaplin16 <chaplin16@orange.fr>
2021-08-24 10:58:54 +0200

    lien sur motifs

--- js/teeshirt_design .js
+++ js/teeshirt_design .js
@@ -3,36 +3,43 @@ const teeshirt_design = [
     {
         id:1,
         attachment:"images/teeshirt_design/Canevas_oh-artwork.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     },
     {
         id:2,
         attachment:"images/teeshirt_design/Shirt.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     },
     {
         id:3,
         attachment:"images/teeshirt_design/Bete_roudil.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     },
     {
         id:4,
         attachment:"images/teeshirt_design/artho.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     },
     {
         id:5,
         attachment:"images/teeshirt_design/encre_acrylique_contrast.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     },
     {
         id:6,
         attachment:"images/teeshirt_design/Ras-Invert.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     },
     {
         id:7,
         attachment:"images/teeshirt_design/reve_aquatic_de larbre.jpg",
+        link:"https://www.brunoroudil.com/",
         alt:"photo"
     }
 ]
