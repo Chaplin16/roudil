ded8611c8e39c32939bdf4c83e5bb18a64d4cd7a
Chaplin16 <chaplin16@orange.fr>
2021-09-28 23:26:11 +0200

    verif wave

--- js/teeshirt_men.js
+++ js/teeshirt_men.js
@@ -4,42 +4,42 @@ const teeshirt_men = [
         id:1,
         attachment:"images/tee-shirt-men/artho_homme_site_hung.jpg",
         link:"https://www.brunoroudil.com/shop/oxl0izm0z600fxoe92idnt5mkzhiia-csazk-pgzth-9tfz9",
-        alt:"tee-shirt blanc pour homme 'Arthocondries' en noir, non porté"
+        alt:"tee-shirt blanc pour homme en noir 100% coton bio à la galerie ROUDIL Bruno"
     },
     {
         id:2,
         attachment:"images/tee-shirt-men/Ras_Homme_on.jpg",
         link:"https://www.brunoroudil.com/shop/oxl0izm0z600fxoe92idnt5mkzhiia-9hgry-begdp",
-        alt:"tee-shirt noir pour homme 'RAS' en blanc"
+        alt:"tee-shirt noir pour homme à Aigues-Mortes"
     },
     {
         id:3,
         attachment:"images/tee-shirt-men/Que_Musique_Noir.jpg",
         link:"https://www.brunoroudil.com/shop/oxl0izm0z600fxoe92idnt5mkzhiia-9hgry",
-        alt:"tee-shirt noir pour homme avec instrument de musique en blanc"
+        alt:"tee-shirt noir pour homme avec instrument par ROUDIL "
     },
     {
         id:4,
         attachment:"images/tee-shirt-men/Que_Musique_Blanc.jpg",
         link:"https://www.brunoroudil.com/shop/oxl0izm0z600fxoe92idnt5mkzhiia-csazk-pgzth",
-        alt:"tee-shirt blanc pour homme avec instrument de musique en noir"
+        alt:"tee-shirt pour homme avec instrument de musique à Aigues-Mortesr"
     },
     {
         id:5,
         attachment:"images/tee-shirt-men/Oh_Oh_Oh_Noire_T-Shirt.jpg",
         link:"https://www.brunoroudil.com/shop/wkg86xhehkslu20yig7ol81dloih6e-bfw7d-c9lp2-wgmpk",
-        alt:"tee-shirt noir pour homme avec silhouette blanche"
+        alt:"tee-shirt pour homme avec silhouette blanche par ROUDIL"
     },
     {
         id:6,
         attachment:"images/tee-shirt-men/Bete_Homme_On.jpg",
         link:"https://www.brunoroudil.com/shop/oxl0izm0z600fxoe92idnt5mkzhiia-csazk-pgzth-9tfz9-6zbsf",
-        alt:"tee-shirt blanc pour homme avec la tête d'une bête en noir"
+        alt:"tee-shirt blanc pour homme avec une bête crée par ROUDIL exposé à Aigues-Mortes"
     },
     {
         id:7,
         attachment:"images/tee-shirt-men/Landscape.jpg",
         link:"https://www.brunoroudil.com/shop/wkg86xhehkslu20yig7ol81dloih6e-bfw7d-62ds8-c5dwl",
-        alt:"tee-shirt noir pour homme avec un paysage ondulé en blanc"
+        alt:"tee-shirt noir pour homme avec un paysage ondulé en coton 100% bio"
     }
 ]
--- js/teeshirt_women.js
+++ js/teeshirt_women.js
@@ -4,43 +4,42 @@ const teeshirt_women = [
         id:1,
         attachment:"images/tee-shirt-women/Paysage_Femme_T-Shirt_Bruno_Roudil.jpg",
         link:"https://www.brunoroudil.com/shop/paysage-ondul-taille-s-t-shirt-artiste",
-        alt:"teeshirt noire pour femme avec un paysage blanc"
+        alt:"teeshirt crée par Bruno ROUDIL pour femme avec un paysage blanc"
     },
     {
         id:2,
         attachment:"images/tee-shirt-women/ORTHO_Femme_front_T-Shirt_Bruno_Roudil.jpg",
         link:"https://www.brunoroudil.com/shop/sigles-ecritures-4266-femme-taille-s-n3ch4-xcpgd",
-        alt:"tesshirt blanc pour femme 'Arthocondries' "
+        alt:"teeshirt  cintré tendance ROUDIL pour femme 'Arthocondries' "
     },
     {
         id:3,
         attachment:"images/tee-shirt-women/OH_Femme_T-Shirt_Bruno_Roudil.jpg",
         link:"https://www.brunoroudil.com/shop/paysage-ondul-taille-s-pd3g4",
-        alt:"teeshirt noir pour femme avec haut d'une silhouette blanche "
+        alt:"teeshirt  pour femme avec silhouette blanche à Aigues-Mortes"
     },
     {
         id:4,
         attachment:"images/tee-shirt-women/Bete_Femme_T-Shirt_Bruno_Roudil_Front.jpg",
         link:"https://www.brunoroudil.com/shop/sigles-ecritures-4266-femme-taille-s-8hp92"
-,        alt:"teeshirt blanc pour femme avec une bête noire "
+,        alt:"teeshirt pour femme par Bruno ROUDIL avec une bête noire"
     },
     {
         id:5,
         attachment:"images/tee-shirt-women/Arbre_Femme_T-Shirt_Bruno_Roudil.jpg",
         link:"https://www.brunoroudil.com/shop/paysage-ondul-taille-s-pd3g4-sxnc2-3kskc",
-        alt:"teeshirt noir pour femme avec un arbre blanc"
+        alt:"teeshirt noir 100% coton bio  pour femme avec un arbre blanc par ROUDIL"
     },
     {
         id:6,
         attachment:"images/tee-shirt-women/Ras_Femme_T-Shirt_Bruno_Roudil.jpg",
         link:"https://www.brunoroudil.com/shop/paysage-ondul-taille-s-pd3g4-sxnc2",
-        alt:"teeshirt noir 'RAS'"
+        alt:"teeshirt noir 'RAS'à la galerie ROUDIL"
     },
     {
         id:7,
         attachment:"images/tee-shirt-women/Que_Musique_Blanc_T-Shirt_Bruno_Roudil.jpg",
         link:"https://www.brunoroudil.com/shop/sigles-ecritures-4266-femme-taille-s-n3ch4",
-        alt:"teeshirt blanc avec musique en noir"
+        alt:"teeshirt blanc avec musique en noir à Aigues-Mortes "
     }
-
 ]
